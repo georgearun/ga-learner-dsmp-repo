56f83508fa1e7b572f8fb9b95c356c0711c9bb32
ArunGeorge <georgearun96@gmail.com>
2018-12-15 21:28:26 +0530

    Code file created.

---
+++ Visualising-the-data/code.py
@@ -0,0 +1,71 @@
+# --------------
+#Importing header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Code starts here
+data = pd.read_csv(path)
+loan_status = data['Loan_Status'].value_counts()
+plt.bar(loan_status.index,loan_status.values)
+plt.show()
+
+
+# --------------
+#Code starts here
+property_and_loan = data.groupby(['Property_Area','Loan_Status']).size().unstack()
+property_and_loan.plot(kind='bar',rot=45).set(xlabel='Property Area',ylabel='Loan Status')
+
+
+# --------------
+#Code starts here
+education_and_loan = data.groupby(['Education','Loan_Status']).size().unstack()
+
+education_and_loan.plot(kind='bar',rot=45).set(xlabel='Education Status',ylabel='Loan Status')
+
+
+# --------------
+#Code starts here
+graduate = data[data['Education']=='Graduate']
+
+not_graduate = data[data['Education']=='Not Graduate']
+
+graduate['LoanAmount'].plot(kind='density',label='Graduate')
+
+not_graduate['LoanAmount'].plot(kind='density',label='Not Graduate')
+
+#Code ends here
+
+#For automatic legend display
+plt.legend()
+
+
+# --------------
+#Code starts here
+#Setting up the subplots
+fig, (ax_1, ax_2,ax_3) = plt.subplots(1,3, figsize=(20,8))
+
+#Plotting scatter plot
+ax_1.scatter(data['ApplicantIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_1.set(title='Applicant Income')
+
+
+#Plotting scatter plot
+ax_2.scatter(data['CoapplicantIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_2.set(title='Coapplicant Income')
+
+
+#Creating a new column 'TotalIncome'
+data['TotalIncome']= data['ApplicantIncome']+ data['CoapplicantIncome']
+
+#Plotting scatter plot
+ax_3.scatter(data['TotalIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_3.set(title='Total Income')
+
+
